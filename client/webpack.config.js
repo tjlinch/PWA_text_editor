00051cd30a3b746f68f4be07cc6416b10f64f221
tjlinch <tjlinch@gmail.com>
2022-10-04 23:25:44 -0600

    set up webpack config, not creating dist directory on npm run build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { Template } = require('webpack');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -19,11 +20,51 @@ module.exports = () => {
     },
     plugins: [
       
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor',
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Progressive Web App Text Editor',
+        short_name: 'TextPWA',
+        description: 'A text editor featuring multiple PWA concepts',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        start_url: './',
+        publicPath: './',
+        icons: [{
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        }]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-tuntime'],
+            }
+          }
+        }
       ],
     },
   };
